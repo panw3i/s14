a87e7d0d7d80d076fdc1237668553d075c1fb199
panw3i <panw3i@gmail.com>
2017-03-29 20:43:10 +0800

    day2

--- day1/sys_mod.py
+++ day1/sys_mod.py
@@ -1,4 +1,18 @@
 import sys
 
 # 打印系统资源查找路径
+
+
 print(sys.path)
+
+for i in sys.path:
+    print(i)
+
+
+
+# /Users/yons/PycharmProjects/s14/day1
+# /Users/yons/PycharmProjects/s14
+# /Library/Frameworks/Python.framework/Versions/3.6/lib/python36.zip
+# /Library/Frameworks/Python.framework/Versions/3.6/lib/python3.6
+# /Library/Frameworks/Python.framework/Versions/3.6/lib/python3.6/lib-dynload
+# /Library/Frameworks/Python.framework/Versions/3.6/lib/python3.6/site-packages
