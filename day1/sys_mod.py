0613cdfdec4b0f96bd21a574edbe71d177292ea0
panw3i <panw3i@gmail.com>
2017-03-29 20:51:16 +0800

    sys argv

--- day1/sys_mod.py
+++ day1/sys_mod.py
@@ -2,17 +2,19 @@ import sys
 
 # 打印系统资源查找路径
 
+#
+# print(sys.path)
+print(sys.argv)
+print(sys.argv[0])  # 代表相当路径
+print(sys.argv[1])  # 第一个参数
+print(sys.argv[2])  # 打印第二个参数
 
-print(sys.path)
-
-for i in sys.path:
-    print(i)
 
 
 
 # /Users/yons/PycharmProjects/s14/day1
 # /Users/yons/PycharmProjects/s14
 # /Library/Frameworks/Python.framework/Versions/3.6/lib/python36.zip
-# /Library/Frameworks/Python.framework/Versions/3.6/lib/python3.6
+# /Library/Frameworks/Python.framework/Versions/3.6/lib/python3.6 标准库
 # /Library/Frameworks/Python.framework/Versions/3.6/lib/python3.6/lib-dynload
-# /Library/Frameworks/Python.framework/Versions/3.6/lib/python3.6/site-packages
+# /Library/Frameworks/Python.framework/Versions/3.6/lib/python3.6/site-packages 外部库
