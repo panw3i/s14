de61c50efde83b14da2e0eaa4471192da8588c40
panw3i <panw3i@gmail.com>
2017-04-06 19:00:59 +0800

    类属性为序列时,作为实例调用类属性调用并不能改变其内存地址

---
+++ day6/ck.py
@@ -0,0 +1,20 @@
+class Foo():
+    n ="类变量"
+    a_list = [1,2,3]
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+    def buy(self):
+        print("buy")
+
+r1  = Foo("pan",90)
+r2  = Foo("li",89)
+r1.buy()
+r1.n  = "修改类变量"
+r1.a_list.append([3,4])
+print(r1.n)
+print(r2.n)
+print(r2.a_list)
+
+# 如果类属性是序列,作为实例的属性时,传入的是列表的地址
